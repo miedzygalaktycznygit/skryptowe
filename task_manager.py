72c89efc03a5acdcdce67fcb4b07cded3d8db8c3
miedzygalaktycznygit <162834563+miedzygalaktycznygit@users.noreply.github.com>
2025-04-14 20:04:36 +0200

    Update task_manager.py

--- task_manager.py
+++ task_manager.py
@@ -25,7 +25,7 @@ class TaskManager:
             "task_name" : description,
             "is_done" : False,
             "created" : datetime.now().isoformat(),
-            "dateline" : deadline
+            "deadline" : deadline
         }
         if any(t["task_name"] == task["task_name"] for t in self.tasks):
             print("this task is arleady in list")
@@ -36,3 +36,29 @@ class TaskManager:
     def list_tasks(self):
         for i in range(len(self.tasks)):
             print(self.tasks[i])
+
+    def get_tasks_due_today(self):
+        time = date.today()
+        for task in self.tasks:
+            if task["deadline"]:
+                try:
+                    deadline_date = datetime.fromisoformat(task["deadline"]).date()
+                    if deadline_date == time:
+                        print("Those are Your tasks for today:\n")
+                        print(task)
+                except ValueError:
+                    print("Valid usage of module")
+
+    def delete_task(self, index):
+        removed = self.tasks.pop(index)
+        self._save_tasks()
+        print("You deleted this task: ")
+        print(removed)
+        print("List after removal:")
+        self.list_tasks()
+
+    def mark_done(self, index):
+        self.tasks[index]["is_done"] = True
+    
+            
+    
