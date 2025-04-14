4deabfd20cfb48af0825ed4f1933c988de1bb579
miedzygalaktycznygit <162834563+miedzygalaktycznygit@users.noreply.github.com>
2025-04-14 20:05:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from task_manager import TaskManager
 
 manager = TaskManager()
 
-manager.add_task("praca domowa", deadline="2025-02-10")
+manager.add_task("coś nowego", deadline="2025-04-14")
+
+manager.mark_done(0)
 
 manager.list_tasks()
+
+manager.get_tasks_due_today()
+
+#manager.delete_task(0)
